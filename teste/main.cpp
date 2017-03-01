adf35f25d8ec4ad5b94f3381a473ea6093c3ba3d
Salazar <francisco.salazar.neto@gmail.com>
2017-02-28 22:11:51 -0300

    Mudei meu teste

--- teste/main.cpp
+++ teste/main.cpp
@@ -1,9 +1,14 @@
 #include <iostream>
-
+#include <iomanip>
 using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    double x = 33.52303;
+
+    cout << fixed << showpoint;
+    cout << setprecision(2) ;
+
+    cout << x ;
     return 0;
 }
